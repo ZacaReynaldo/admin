51e7fc5a0529bc34d7abe5944fda3258e1968b2e
Jack Biller <jackbiller19@gmail.com>
2021-03-14 22:46:17 -0300

    Ajustes Gerais

--- controller/login.php
+++ controller/login.php
@@ -1,5 +1,17 @@
 <?php
 
+date_default_timezone_set('America/Sao_Paulo');
+
+// Enviar o email para redefinir a senha
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require '../vendor/autoload.php';
+
 include "funcoes.php";
 $pdo = getConection();
 
@@ -117,16 +129,7 @@ if (!empty($_POST['email'])) {
 		return;
 	}
 
-	// Enviar o email para redefinir a senha
-	// Import PHPMailer classes into the global namespace
-	// These must be at the top of your script, not inside a function
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\SMTP;
-	use PHPMailer\PHPMailer\Exception;
-
-	// Load Composer's autoloader
-	require 'vendor/autoload.php';
-
+	$configEnv = json_decode(file_get_contents('../config.env'));
 	// Instantiation and passing `true` enables exceptions
 	$mail = new PHPMailer(true);
 
@@ -134,19 +137,25 @@ if (!empty($_POST['email'])) {
 		// Server settings
 		$mail->SMTPDebug 	= SMTP::DEBUG_SERVER; 						// Enable verbose debug output
 		$mail->isSMTP(); 												// Send using SMTP
-		$mail->Host 		= 'smtp.gmail.com'; 						// Set the SMTP server to send through
+		$mail->Host 		= $configEnv->password_reset__host; 		// Set the SMTP server to send through
 		$mail->SMTPAuth 	= true; 									// Enable SMTP authentication
-		$mail->Username 	= 'email@gmail.com'; 						// SMTP username
-		$mail->Password 	= 'senha'; 									// SMTP password
-		// $mail->SMTPSecure 	= PHPMailer::ENCRYPTION_SMTPS; 			// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-		// $mail->SMTPSecure 	= PHPMailer::ENCRYPTION_STARTTLS; 		// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-		$mail->SMTPSecure 	= 'ssl';
-		// $mail->Port 		= 587; 										// TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-		$mail->Port 		= 465; 										// SSL port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		$mail->Username 	= $configEnv->password_reset__email; 		// SMTP username
+		$mail->Password 	= $configEnv->password_reset__psw; 			// SMTP password
+		
+		if ($configEnv->password_reset__isGmail) { 
+			// $mail->SMTPSecure 	= PHPMailer::ENCRYPTION_SMTPS; 		// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+			$mail->SMTPSecure 	= 'ssl';
+			$mail->Port 		= 465;
+			// SSL port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		} else { 
+			$mail->SMTPSecure 	= PHPMailer::ENCRYPTION_STARTTLS; 		// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+			$mail->Port 		= 587;
+			// TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		}
 
 		// Recipients
-		$mail->setFrom('email@gmail.com', 'Emissor');
-		$mail->addAddress('email@gmail.com', $usuario->get('NOME')); 	// Add a recipient
+		$mail->setFrom($configEnv->password_reset__email, $configEnv->password_reset__name);
+		$mail->addAddress($email, $usuario->get('NOME')); 				// Add a recipient
 		// $mail->addAddress('ellen@example.com'); 						// Name is optional
 		// $mail->addReplyTo('info@example.com', 'Information');
 		// $mail->addCC('cc@example.com');
@@ -158,9 +167,9 @@ if (!empty($_POST['email'])) {
 
 		// Content
 		$mail->isHTML(true); 											// Set email format to HTML
-		$mail->Subject 	= 'Here is the subject';
-		$mail->Body 	= 'This is the HTML message body <b>in bold!</b>';
-		$mail->AltBody 	= 'This is the body in plain text for non-HTML mail clients';
+		$mail->Subject 	= 'Esse E-mail é para recuperar senha';
+		$mail->Body 	= ''; 											// Is HTML
+		$mail->AltBody 	= ''; 											// Text Plain
 
 		$mail->send();
 		echo 'Mensagem foi enviada com sucesso';
